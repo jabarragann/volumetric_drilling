09c612539e17088d7b81299d39eb0d1463082409
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-03-31 22:45:27 -0400

    cube anatomy generator

---
+++ scripts/AnatomyGeneration/README.md
@@ -0,0 +1,3 @@
+# Anatomy Generation scripts
+
+Create simple anatomies such as cubes and spheres that can be used to test the SDF accuracy
---
+++ scripts/AnatomyGeneration/generate_cube.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import argparse
+from pathlib import Path
+
+if __name__ == "__main__":
+
+    # fmt: off
+    parser = argparse.ArgumentParser(description="Description of your program")
+    parser.add_argument("-s", "--resolution", help="Image resolution", default=512)
+    parser.add_argument("-c", "--cuberes", help="Cube resolution", default=256)
+    parser.add_argument("-f", "--dirname", help="directory name", default="cube512_256")
+    parser.add_argument("-r", "--root", help="Location where you want to save the images",
+                        default="resources/volumes/")
+    # fmt: on
+    args = parser.parse_args()
+
+    # define path
+    root: Path = Path(args.root) / args.dirname
+
+    if not root.exists():
+        root.mkdir(parents=True)
+
+    ans = input(f"storing images in: {root} (y/n) ")
+    if ans == "n":
+        exit(0)
+    else:
+        img_res = args.resolution
+        cube_res = args.cuberes
+        init_p = int((img_res - cube_res) / 2)
+        counter = 0
+        with open((root / args.dirname).with_suffix(".txt"), "w") as f:
+
+            for i in range(args.resolution):
+                img = np.zeros((args.resolution, args.resolution, 4), dtype=np.uint8)
+                # img[:, :, -1] = 25
+                img = cv2.rectangle(
+                    img, (init_p, init_p), (init_p + cube_res, init_p + cube_res), (255, 255, 255, 255), -1
+                )
+                img_name = f"plane00{counter}.png"
+                f.write(img_name + "\n")
+                cv2.imwrite(str(root / img_name), img)
+                counter += 1
+
+                # test = cv2.imread(str(root / img_name), cv2.IMREAD_UNCHANGED)
+                # print(test[100, 100])
+                # break
