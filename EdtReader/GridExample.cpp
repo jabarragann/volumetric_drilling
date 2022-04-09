0388ba614d5c399d5cf3846da407514e386ea37a
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-04-09 13:52:10 -0400

    Merge new EdtReader into feature_force

--- EdtReader/GridExample.cpp
+++ EdtReader/GridExample.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Grid example!" << endl;
-
-    Array3d<float> grid(3, 3, 3);
+    cout << "Grid structures tests!" << endl;
+    unsigned int res[3] = {5, 3, 2};
+    Array3d<float> grid(res);
     float counter = 0.0;
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < res[2]; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < res[1]; j++)
         {
-            for (int k = 0; k < 3; k++)
+            for (int k = 0; k < res[0]; k++)
             {
                 grid(k, j, i) = counter;
                 counter++;
@@ -21,10 +21,28 @@ int main()
         }
     }
 
+    printf("Accessing data via pointer.\n");
     float *ptr = grid.data;
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 6; i++)
     {
         printf("%d %.6f\n", i, *ptr);
         ptr++;
+    }
+    printf("\n");
+
+    // Test data accessing methods
+    printf("Test data accessing methods.\n");
+    printf("-----------------------\n");
+    for (int z = 0; z < res[2]; z++)
+    {
+        for (int y = 0; y < res[1]; y++)
+        {
+            for (int x = 0; x < res[0]; x++)
+            {
+                printf("%4.1f ", grid(x, y, z));
+            }
+            printf("\n");
+        }
+        printf("-----------------------\n");
     }
 }
