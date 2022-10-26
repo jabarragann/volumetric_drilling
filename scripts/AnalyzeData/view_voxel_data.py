d08804f074ed44ef698ff6d6c7159504a8400a0f
Juan Antonio Barragan <jbarrag3@jh.edu>
2022-10-26 15:49:15 -0400

    Patch 2 for recording script
    - Removed voxels are stored now as a 2d numpy array
    - Fix error due to interrupting writing with a Ctrl+c.
    - Current code has multiple global variables.
    
    TODO: rewrite script into classes

--- scripts/AnalyzeData/view_voxel_data.py
+++ scripts/AnalyzeData/view_voxel_data.py
@@ -1,5 +1,8 @@
+from re import I
 import h5py
 import numpy as np
+from pathlib import Path
+import pandas as pd
 
 np.set_printoptions(precision=3)
 
@@ -11,18 +14,31 @@ def get_all(name):
 
 def main(f):
     f.visit(get_all)
+    print()
 
-    ts_dataset = f["voxels_removed/time_stamp"]
-    voxel_color_dataset = f["voxels_removed/voxel_color"]
+    ts_dataset = f["voxels_removed/voxel_time_stamp"]
+    voxel_color = f["voxels_removed/voxel_color"][()]
+    voxel_index = f["voxels_removed/voxel_removed"][()]
 
-    print(ts_dataset.shape)
-    print(voxel_color_dataset.shape)
+    # Load hdf5 into dataframe
+    voxel_color = pd.DataFrame(voxel_color,columns=['ts','r','g','b','a'])
+    voxel_index= pd.DataFrame(voxel_index,columns=['ts','x','y','z'])
 
-    for idx in range(5):
-        print(f"{idx}-{ts_dataset[idx]}\n{voxel_color_dataset[idx,:,:]}")
+    print("timestamps", ts_dataset.shape)
+    print("voxel color", voxel_color.shape)
+    print("voxel index", voxel_index.shape)
 
+    print(f"voxel df\n{voxel_color.head()}\n")
+
+    print(f"voxels removed at step 5000 {voxel_color.loc[voxel_color['ts']==5000].shape}")
+    print(f"voxels removed at step 8000 {voxel_color.loc[voxel_color['ts']==8000].shape}")
+    
 
 if __name__ == "__main__":
 
-    with h5py.File("volumetric_drilling/scripts/data-sample/20221024_130138.hdf5", "r") as f:
+    path = Path("scripts/data-sample/new_rec_format.hdf5")
+    if not path.exists():
+        print("path does not exists")
+
+    with h5py.File(path, "r") as f:
         main(f)
