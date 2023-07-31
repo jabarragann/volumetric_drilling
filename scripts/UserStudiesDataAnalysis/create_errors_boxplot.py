bd4023483ebbae442cd973cfd29f2c5aabc3b301
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-07-31 15:20:20 -0400

    Modify analysis script for IROS 2023 final version.
    - Modified error plot y-axis from number of unintended removed voxels to
    mm^3 of unintended drilling.
    - Added data file that was used to generate the paper plots

--- scripts/UserStudiesDataAnalysis/create_errors_boxplot.py
+++ scripts/UserStudiesDataAnalysis/create_errors_boxplot.py
@@ -21,8 +21,10 @@ adjust_params = dict(top=0.88, bottom=0.18, left=0.125, right=0.9, hspace=0.2, w
 
 
 def main():
+    cf = Path(__file__).parent.resolve()
+    root = cf / "IROS2023_paper_data/final_objective_metrics.csv"
     # root = Path("/home/juan1995/research_juan/cisII_SDF_project/Data/RedCap/final_objective_metrics.csv")
-    root = Path("/home/juan1995/research_juan/cisII_SDF_project/Data/UserStudy2_IROS/results.csv")
+    # root = Path("/home/juan1995/research_juan/cisII_SDF_project/Data/UserStudy2_IROS/results.csv")
     df = pd.read_csv(root)
 
     voxel_cols = [col for col in df.columns if "voxel" in col]
@@ -65,20 +67,23 @@ def main():
     #######################
     ## errors plot
     #######################
+    df["total_errors"] =  df["total_errors"] * 0.007 
 
     ax: plt.Axes
     fig, ax = plt.subplots(1)
     fig.set_tight_layout(True)
     fig.subplots_adjust(**adjust_params)
-    sns.boxplot(df, x="guidance", y="total_errors", ax=ax)
-    sns.swarmplot(df, x="guidance", y="total_errors", color="black", ax=ax)
+    sns.boxplot(df, x="guidance", y="total_errors", ax=ax, order=["Baseline","Visual","Haptic","Audio"])
+    sns.swarmplot(df, x="guidance", y="total_errors", color="black", ax=ax, order=["Baseline","Visual","Haptic","Audio"])
 
     # Remove repeated labels
     # handles, labels = ax.get_legend_handles_labels()
     # by_label = dict(zip(labels[:2], handles[:2]))
     # ax.legend(by_label.values(), by_label.keys())
     ax.set_xlabel("Feedback modality", labelpad=10)
-    ax.set_ylabel("Unintended voxels removed", labelpad=10)
+    ax.set_ylabel("$mm^3$ of unintended drilling", labelpad=10)
+
+    # "Unintended voxels removed"
     ax.grid(color="black", alpha=0.5, axis="y")
 
     #######################
