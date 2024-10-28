4d27fc5752ab613af007792b2f580ed8120b97dd
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-10-28 13:49:44 -0400

    Add script to create colormap

---
+++ scripts/AnalyzeData/create_colormap_bar.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import matplotlib.colorbar as colorbar
+import matplotlib.colors as colors
+import numpy as np
+
+max_val = 1.8
+min_val = -1.8
+num_of_ticks = 9
+
+# Create a figure and axis for the color bar with extra padding
+fig, ax = plt.subplots(figsize=(1.5, 6))  # Increase width for left/right padding
+
+# Define the colormap and normalization for the color bar
+cmap = plt.get_cmap("turbo")  # Replace with your preferred colormap
+norm = colors.Normalize(vmin=min_val, vmax=max_val)
+
+# Create the color bar
+cb = colorbar.ColorbarBase(ax, cmap=cmap, norm=norm, orientation="vertical")
+
+# Set color bar label with increased font size and unit
+cb.set_label("SDF distance to reference mesh (mm)", fontsize=14, labelpad=20)
+cb.ax.tick_params(labelsize=12)  # Increase tick font size
+
+ticks = np.linspace(min_val, max_val, num_of_ticks).tolist()
+ticks_str = [f"{t:0.1f}" for t in ticks]
+cb.set_ticks(ticks)  # Explicitly set tick positions
+cb.set_ticklabels(ticks_str)  # Set the labels accordingly
+
+print(ticks)
+print(ticks_str)
+
+# Add padding around the color bar
+plt.subplots_adjust(left=0.3, right=0.7)
+
+# Save the color bar as an image if needed
+plt.savefig("color_map_bar.png", dpi=300, bbox_inches="tight", transparent=True)
+plt.show()
